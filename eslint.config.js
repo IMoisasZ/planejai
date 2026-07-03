87d63d4faaa07bc5220b888084b0a42e224b8462
moisas <mopri08@gmail.com>
2026-07-02 23:44:26 -0300

    installed the dependencies; created the styles with tailwind; created the inicial routes with react-router-dom

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,42 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import unusedImports from 'eslint-plugin-unused-imports'
+import sortImports from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.app.json'],
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'unused-imports': unusedImports,
+      'simple-import-sort': sortImports,
+    },
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tsPlugin.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
     ],
-    languageOptions: {
-      globals: globals.browser,
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'unused-imports/no-unused-imports': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+
 function App() {
-	return <h1>Hello, Vite</h1>
+	return (
+		<RouterProvider router={router} />
+	)
 }
 
 export default App
